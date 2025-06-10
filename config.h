689f68ad3eabe6c6b755aa0f42beba61fccfb8aa
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 18:02:17 +0200

    create config file

---
+++ config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+constexpr int IMAGE_SIZE = 28;
+constexpr int INPUT_SIZE = 784;
+constexpr int BATCH_SIZE = 32; // Unused for now
+constexpr int EPOCHS_NUMBER = 10;
+constexpr double LEARNING_RATE = 0.001;
+constexpr int OUTPUT_NUMBER = 10;
+
+#include <string_view>
+constexpr std::string_view WEIGHTS_PATH = "weights.csv";
+
+constexpr std::string_view DATASET_TEST_PATH = "dataset/mnist_test.csv";
+constexpr std::string_view DATASET_TRAIN_PATH = "dataset/mnist_train.csv";
+
+#endif //CONFIG_H
