f60d5d68c3f9b1f51789426115586b8dd620b38a
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 18:10:21 +0200

    use constexpr in other files

--- train.cpp
+++ train.cpp
@@ -16,7 +16,7 @@ int main() {
     // show_dataset_element(dataset_train[12]);
 
     std::cout << "===================> Begin to train the model" << std::endl;
-    const std::vector<std::vector<double>> model = get_trained_model(dataset_train, 10);
+    const std::vector<std::vector<double>> model = get_trained_model(dataset_train);
 
     std::cout << "===================> Begin to save the weights" << std::endl;
     save_weights(model, "weights.csv");
