2e672cd9867f9f4c7aaea12dfcd8a7db84b2a8a0
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 15:28:14 +0200

    use train and save in train script

--- train.cpp
+++ train.cpp
@@ -10,12 +10,19 @@
 
 int main() {
 
-    std::cout << "===> Begin to load dataset" << std::endl;
-
+    std::cout << "===================> Begin to load dataset" << std::endl;
     std::vector<std::pair<std::vector<uint8_t>, uint8_t>>  dataset_train = readCSV("dataset/mnist_train.csv");
-    std::cout << "=> Finished loading train set, size " << dataset_train.size() << " . " << std::endl;
+    std::cout << "Finished loading train set, size " << dataset_train.size() << " . " << std::endl;
 
-    show_dataset_element(dataset_train[12]);
+    //show_dataset_element(dataset_train[12]);
+
+    std::cout << "===================> Begin to train the model" << std::endl;
+    std::vector<std::vector<double>> model = get_trained_model(dataset_train, 10);
+    
+
+    std::cout << "===================> Begin to save the weights" << std::endl;
+    save_weights(model, "weights.csv");
+
 
     return 0;
 }
