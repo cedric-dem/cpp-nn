b34a296a4fbcaf6c17020001237a6da1f31f377d
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 22:47:35 +0200

    minor fixes

--- functions.cpp
+++ functions.cpp
@@ -178,11 +178,11 @@ void batch(int current_batch_index, std::vector<std::pair<std::vector<uint8_t>, 
 
         // prediction
         std::vector<double> raw_data = multiply_input_vector_with_weights(x, current_weights);
-        y_hat_vector = biggest_1_else_0(raw_data);
-        // y_hat_vector = raw_data;
+
+        // y_hat_vector = biggest_1_else_0(raw_data);
         // y_hat_vector = sigmoid(raw_data);
-        //  y_hat_vector = relu(raw_data);
-        //  y_hat_vector = binary_f(raw_data);
+        // y_hat_vector = relu(raw_data);
+        y_hat_vector = f_binary(raw_data);
 
         // if not good (or always ?)
 
