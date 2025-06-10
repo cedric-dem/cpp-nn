b13258ddaaf3697c6d862b4f257758d6b21eda2b
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 15:10:08 +0200

    create get_random_vector function ton initialize weights

--- functions.cpp
+++ functions.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+#include <random>
 #include "functions.h"
 
 
@@ -74,3 +75,18 @@ void show_dataset_element(const std::pair<std::vector<uint8_t>, uint8_t> dataset
     display_matrix(dataset_elem.first, 28, 28);
 }
 
+
+std::vector<double> get_random_vector(const int size) {
+    std::vector<double> vec(size);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<> d(0.0, 1.0);
+
+    for (int i = 0; i < size; ++i) {
+        vec[i] = d(gen);
+    }
+
+    return vec;
+
+}
--- train.cpp
+++ train.cpp
@@ -4,7 +4,9 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+
 #include "functions.h"
+
 
 int main() {
 
