8e6e6885a13689aa418f8dff8dbf0a7e57136493
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 14:37:53 +0200

    fix labels not retrieved correctly

--- test.cpp
+++ test.cpp
@@ -13,7 +13,7 @@ int main() {
     std::vector<std::pair<std::vector<uint8_t>, uint8_t>>  dataset_test = readCSV("dataset/mnist_test.csv");
     std::cout << "=> Finished loading test set, size " << dataset_test.size() << " . " << std::endl;
 
-    show_dataset_element(dataset_test[12]);
+    show_dataset_element(dataset_test[19]);
 
     return 0;
 }
