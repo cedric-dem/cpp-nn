31b39f93be8cbedb96cbd8f944a7480a88d07ccf
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 14:16:06 +0200

    create train and test file instead of main

---
+++ train.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include "functions.h"
+
+int main() {
+
+    std::cout << "===> Begin to load dataset" << std::endl;
+    
+    std::vector<std::pair<std::vector<uint8_t>, uint8_t>>  dataset_test = readCSV("dataset/mnist_test.csv");
+    std::cout << "=> Finished loading test set, size " << dataset_test.size() << " . " << std::endl;
+    
+    // std::vector<std::pair<std::vector<uint8_t>, uint8_t>>  dataset_train = readCSV("dataset/mnist_train.csv");
+    // std::cout <<"=> Finished loading train set, size " << dataset_train.size() << " . " << std::endl;
+
+    show_dataset_element(dataset_test[12]);
+
+    return 0;
+}
