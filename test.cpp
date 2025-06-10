74f7d76c7b8f41eefeb2dfa3a501ae3417ba6e00
cedric <demuelenaere.cedric@gmail.com>
2025-06-10 17:52:51 +0200

    apply code formatter

--- test.cpp
+++ test.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 #include "functions.h"
 
 int main() {
 
     std::cout << "===================> Begin to load dataset" << std::endl;
-    const std::vector<std::pair<std::vector<uint8_t>, uint8_t>>  dataset_test = readDataset("dataset/mnist_test.csv");
+    const std::vector<std::pair<std::vector<uint8_t>, uint8_t>> dataset_test = readDataset("dataset/mnist_test.csv");
     std::cout << "=> Finished loading test set, size " << dataset_test.size() << " . " << std::endl;
 
     // show_dataset_element(dataset_test[19]);
@@ -20,7 +20,6 @@ int main() {
 
     std::cout << "===================> Begin evaluation on the test set" << std::endl;
     evaluate_model(weights, dataset_test);
-
 
     return 0;
 }
