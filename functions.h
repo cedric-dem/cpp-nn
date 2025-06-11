637bbf667198a0228387816e5c0212cd1849b40e
cedric <demuelenaere.cedric@gmail.com>
2025-06-11 15:20:17 +0200

    clean code

--- functions.h
+++ functions.h
@@ -5,30 +5,22 @@
 #include "config.h"
 
 std::vector<DataPoint> readDataset(const std::string &filepath);
-
 WEIGHT_SHAPE readWeights();
+void saveWeights(const WEIGHT_SHAPE &model, const std::string &filepath);
+
+void shuffleDataset(std::vector<DataPoint> &dataset);
+
+void showDatasetElement(DataPoint dataset_elem);
+void displayMatrix(const IMAGE_SHAPE &data, uint8_t size_a, uint8_t size_b);
 
 void displayConfusionMatrix(const std::array<std::array<double, NN_OUTPUT_SIZE>, NN_OUTPUT_SIZE> &data);
 
-void displayMatrix(const IMAGE_SHAPE &data, uint8_t size_a, uint8_t size_b);
-
-void showDatasetElement(DataPoint dataset_elem);
-
-WEIGHT_SHAPE getRandomMatrix();
-
 NeuralNetwork getTrainedModel(std::vector<DataPoint> &dataset_train);
-
-void saveWeights(const WEIGHT_SHAPE &model, const std::string &filepath);
-
+NN_OUTPUT_SHAPE multiplyInputVectorWithWeights(const IMAGE_SHAPE &input_data, const WEIGHT_SHAPE &weights);
 double evaluateModel(const NeuralNetwork &model, const std::vector<DataPoint> &dataset, bool show_confusion_matrix);
 
-int getPrediction(const IMAGE_SHAPE &input_data, const WEIGHT_SHAPE &weights);
-
-NN_OUTPUT_SHAPE multiplyInputVectorWithWeights(const IMAGE_SHAPE &input_data, const WEIGHT_SHAPE &weights);
-
+WEIGHT_SHAPE getRandomMatrix();
 int indexOfMax(const NN_OUTPUT_SHAPE &output);
-
-void shuffleDataset(std::vector<DataPoint> &dataset);
 
 NN_OUTPUT_SHAPE biggest1Else0(const NN_OUTPUT_SHAPE &inp);
 NN_OUTPUT_SHAPE sigmoid(const NN_OUTPUT_SHAPE &inp);
