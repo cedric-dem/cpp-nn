3a1697a008dd043504d322b02a496d18a60a64d0
cedric <demuelenaere.cedric@gmail.com>
2025-06-11 15:17:27 +0200

    move getPrediction inside NeuralNEtwork class

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -14,6 +14,7 @@ class NeuralNetwork {
     WEIGHT_SHAPE getDeltaMatrix(const int start_index, const int end_index, const std::vector<DataPoint> &dataset_train) const;
     void adjustWeights(const WEIGHT_SHAPE &delta_matrix);
     WEIGHT_SHAPE getWeights() const;
+    int getPrediction(const IMAGE_SHAPE &input_data) const;
 };
 
 #endif // NEURALNETWORK_H
