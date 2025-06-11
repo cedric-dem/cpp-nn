9252d6c53d7491e13bd7b2b9b40f20a324392890
cedric <demuelenaere.cedric@gmail.com>
2025-06-11 12:41:25 +0200

    create confusion matrix

--- functions.h
+++ functions.h
@@ -11,6 +11,8 @@ std::vector<DataPoint> readDataset(const std::string &filepath);
 
 WEIGHT_SHAPE readWeights();
 
+void displayConfusionMatrix(const std::array<std::array<double, NN_OUTPUT_SIZE>, NN_OUTPUT_SIZE> &data);
+
 void displayMatrix(const IMAGE_SHAPE &data, uint8_t size_a, uint8_t size_b);
 
 void showDatasetElement(DataPoint dataset_elem);
@@ -21,7 +23,7 @@ WEIGHT_SHAPE getTrainedModel(std::vector<DataPoint> &dataset_train);
 
 void saveWeights(const WEIGHT_SHAPE &model, const std::string &filepath);
 
-double evaluateModel(const WEIGHT_SHAPE &weights, const std::vector<DataPoint> &dataset);
+double evaluateModel(const WEIGHT_SHAPE &weights, const std::vector<DataPoint> &dataset, bool show_confusion_matrix);
 
 int getPrediction(const IMAGE_SHAPE &input_data, const WEIGHT_SHAPE &weights);
 
