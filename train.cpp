f5bf4c3bcbfe6b404f9933e881d36e7675fad7ca
cedric <demuelenaere.cedric@gmail.com>
2025-06-17 21:48:32 +0200

    clean cout

--- train.cpp
+++ train.cpp
@@ -8,16 +8,14 @@
 
 int main() {
 
-    std::cout << "===================> Begin to load dataset" << std::endl;
+    std::cout << "====> Begin to load dataset" << std::endl;
     std::vector<DataPoint> dataset_train = readDataset(DATASET_TRAIN_PATH);
-    std::cout << "Finished loading train set, size " << dataset_train.size() << " . " << std::endl;
+    std::cout << "====> Finished loading train set, size " << dataset_train.size() << " . " << std::endl;
 
-    // show_dataset_element(dataset_train[12]);
-
-    std::cout << "===================> Begin to train the model" << std::endl;
+    std::cout << "====> Begin to train the model" << std::endl;
     const NeuralNetwork model = getTrainedModel(dataset_train);
 
-    std::cout << "===================> Begin to save the weights" << std::endl;
+    std::cout << "====> Begin to save the weights" << std::endl;
     saveWeights(model.getWeights(), "weights.csv");
 
     return 0;
